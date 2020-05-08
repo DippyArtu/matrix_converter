599f314c998fb6d83a6d98a55c34f46aff092a85
DippyArtu <artur.makhnach@gmail.com>
2020-05-09 00:15:47 +0300

    upd

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,50 +1,60 @@
 #include "libft.h"
 #include <stdio.h>
 
-int		main(int argc, char **argv)
+char		*parse_data(int fd, char *line)
 {
-	int		fd;
-	int		out_file;
-	char		*line;
-	char		*tmp;
-	char		*res;
+	int		line_c;
 	int		i;
 	int		j;
-	int		line_c;
+	char		*tmp;
+	char		*res;
+	char		*binary;
 
-	if (argc != 2)
-		exit(0);
-	fd = open(argv[1], O_RDONLY);
-	out_file = open("result", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	binary = (char *)malloc(sizeof(char) * 400);
+	ft_bzero(binary, 400);
 	while ((get_next_line(fd, &line)))
 	{
 		line_c = 0;
 		while (line[line_c] != '\0')
 		{
-			res = (char *)malloc(sizeof(char) * 7);
+			res = (char *)malloc(sizeof(char) * 6);
 			tmp = (char *)malloc(sizeof(char) * 12);
 			ft_bzero(tmp, 12);
-			ft_bzero(res, 7);
+			ft_bzero(res, 6);
 			tmp = ft_strncpy(tmp, &line[line_c], 12);
 			i = 0;
 			j = 0;
 			while (tmp[i])
 			{
-				if (tmp[i] != 32)
+				if (tmp[i] == 48 || tmp[i] == 49)
 				{
 					res[j] = tmp[i];
 					j++;
 				}
 				i++;
 			}
-			free(tmp);
+			binary = ft_strcat(binary, res);
+			binary = ft_strcat(binary, "\n");
 			free(res);
-			ft_putendl_fd(res, out_file);
 			line_c += 12;
 		}
 		free(line);
 	}
+	return(binary);
+}	
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char		*line;
+	char		*binary;
+
+	if (argc != 2)
+		exit(0);
+	fd = open(argv[1], O_RDONLY);
+	binary = parse_data(fd, line);
+	printf("%s", binary);
+	free(binary);
 	close(fd);
-	close(out_file);
 	return(0);
 }
