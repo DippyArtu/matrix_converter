8f356225580f0556131d7a476514408fb9f85758
DippyArtu <artur.makhnach@gmail.com>
2020-05-10 14:19:00 +0300

    conversion done

--- srcs/converter.c
+++ srcs/converter.c
@@ -5,9 +5,12 @@
 
 static void		free_matrix(int ***m)
 {
-	for (int i = 0; i < 6; i++)
+	int		i;
+	int		j;
+
+	for (i = 0; i < 6; i++)
 	{
-		for (int j = 0; j < 6; j++)
+		for (j = 0; j < 6; j++)
 			free(m[i][j]);
 		free(m[i]);
 	}
@@ -18,12 +21,21 @@ static void		free_matrix(int ***m)
 static int		***alloc_matrix(void)
 {
 	int		***matrix;
+	int		i = 0;
+	int		j = 0;
 
 	matrix = (int ***)malloc(sizeof(int **) * 6);
-	for (int i = 0; i < 6; i++)
+	while (i < 7)
+	{
 		matrix[i] = (int **)malloc(sizeof(int *) * 6);
-		for (int j = 0; j < 6; j++)
+		j = 0;
+		while (j < 7)
+		{
 			matrix[i][j] = (int *)malloc(sizeof(int) * 6);
+			j++;
+		}
+		i++;
+	}
 	return(matrix);
 }
 
@@ -42,13 +54,12 @@ static int		bin_to_dec(char *bin)
 	return(res);
 }
 
-//TODO convert this to 3d (6*6*6)
-static void		fill_matrix(int **matrix, int number)
+static void		fill_matrix(int **matrix, int number, int reset)
 {
 	static int	row = 0;
 	static int	col = 0;
 
-	if (row < 6 && col < 5)
+	if (row < 6 && col < 6)
 	{
 		matrix[col][row] = number;
 		row++;
@@ -60,8 +71,12 @@ static void		fill_matrix(int **matrix, int number)
 		matrix[col][row] = number;
 		row++;
 	}
+	if (reset == 35)
+	{
+		row = 0;
+		col = 0;
+	}
 }
-
 
 static int		***parse_data(int fd, char *line)
 {
@@ -70,22 +85,23 @@ static int		***parse_data(int fd, char *line)
 	int		j;
 	char		*tmp;
 	char		*bin;
-	int		***result;
+	int		***matrix;
 	int		nbr;
 
-	int counter = 1;
-	int c_t = 1;
+	int		counter = 0;
+	int 		c_t = 0;
+	int		dim = 0;
 
-	result = alloc_matrix();
+	matrix = alloc_matrix();
 	while ((get_next_line(fd, &line)))
 	{
 		line_c = 0;
-		while (line[line_c] != '\0')
+		while (line[line_c])
 		{
 			bin = (char *)malloc(sizeof(char) * 6);
 			tmp = (char *)malloc(sizeof(char) * 12);
 			ft_bzero(tmp, 12);
-			ft_bzero(res, 6);
+			ft_bzero(bin, 6);
 			tmp = ft_strncpy(tmp, &line[line_c], 12);
 			i = 0;
 			j = 0;
@@ -99,31 +115,25 @@ static int		***parse_data(int fd, char *line)
 				i++;
 			}
 			nbr = bin_to_dec(bin);
-
-			printf("%i\n", nbr);
-			counter++;
-			c_t++;
-
 			if (c_t == 36)
 			{
-				printf("\n------counter: %i-------\n", counter);
 				c_t = 0;
+				dim++;
 			}
+			fill_matrix(matrix[dim], nbr, c_t);
 
-			fill_matrix(result, nbr);
 			free(bin);
 			free(tmp);
 			line_c += 12;
+
+			counter++;
+			c_t++;
 		}
 		free(line);
 	}
 	free(line);
 	line = NULL;
-	
-	printf("\n----------counter: %i----------\n", counter);
-	exit(0);
-
-	return(result);
+	return(matrix);
 }	
 
 int		main(int argc, char **argv)
@@ -137,11 +147,15 @@ int		main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	decimals = parse_data(fd, line);
 
-	for (int i = 0; i < 6; i++)
+	for (int c = 0; c < 6; c++)
 	{
-		printf("\n");
-		for (int j = 0; j < 6; j++)
-			printf("%i ", decimals[i][j]);
+		printf("\n\n");
+		for (int i = 0; i < 6; i++)
+		{
+			printf("\n");
+			for (int j = 0; j < 6; j++)
+				printf("%i ", decimals[c][i][j]);
+		}
 	}
 
 	free_matrix(decimals);
